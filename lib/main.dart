350af27bd0c720e92e320a0f24a7fed57bb690d9
josoavj <josoavonjiniaina13@gmail.com>
2025-08-28 20:20:57 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:navbar_custom/Pages/Homepage.dart';
+import 'Pages/customnav.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      home: CustomNavbar(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:  Homepage(),
     );
   }
 }
