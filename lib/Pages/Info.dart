fc3708feb46d4d246de7c377a201e0984b32e220
josoavj <josoavonjiniaina13@gmail.com>
2025-08-28 20:21:03 +0300

    Update Info.dart

--- lib/Pages/Info.dart
+++ lib/Pages/Info.dart
@@ -1,106 +1,19 @@
+// lib/pages/informations_page.dart
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:navbar_custom/Pages/Files.dart';
-import 'package:navbar_custom/Pages/Homepage.dart';
-import 'package:navbar_custom/Pages/Settings.dart';
 
-class Informations extends StatelessWidget {
-  const Informations({super.key});
+class InformationsPage extends StatelessWidget {
+  const InformationsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 5,
-        backgroundColor: Colors.blueAccent,
-        centerTitle: true,
-        title: Text(
-          "Informations",
-          style: GoogleFonts.poppins(
-            fontSize: 15,
-            color: Colors.white,
-            fontWeight: FontWeight.w800,
-          ),
-        ),
-      ),
-      body: Center(
-        child: Text(
-          "Your Informations",
-          style: GoogleFonts.poppins(
-            fontSize: 20,
-            color: Colors.black,
-            fontWeight: FontWeight.w400,
-          ),
-        ),
-      ),
-      bottomNavigationBar: Container(
-        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.5),
-              blurRadius: 25,
-              offset: const Offset(0, 20),
-            )
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(25),
-          child: NavigationBar(
-            indicatorColor: Colors.blueAccent,
-            elevation: 0,
-            height: 70,
-            selectedIndex: 2, // 'Informations' is at index 2
-            onDestinationSelected: (indexPage) {
-              // Helper function to navigate without transition
-              void navigateWithoutTransition(Widget page) {
-                Navigator.push(
-                  context,
-                  PageRouteBuilder(
-                    pageBuilder: (context, animation1, animation2) => page,
-                    transitionDuration: Duration.zero, // No transition duration
-                    transitionsBuilder:
-                        (context, animation1, animation2, child) {
-                      return child; // Return the child directly, no animation
-                    },
-                  ),
-                );
-              }
-
-              switch (indexPage) {
-                case 0:
-                  navigateWithoutTransition(const Homepage());
-                  break;
-                case 1:
-                  navigateWithoutTransition(const Files());
-                  break;
-                case 2:
-                // This page - no navigation needed as we are already here
-                  break;
-                case 3:
-                  navigateWithoutTransition(Settings());
-                  break;
-              }
-            },
-            destinations: const [
-              NavigationDestination(
-                icon: Icon(Icons.home_filled),
-                label: "Homepage",
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.book_sharp),
-                label: "Files",
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.info_sharp),
-                label: "Informations",
-              ),
-              NavigationDestination(
-                icon: Icon(Icons.settings),
-                label: "Settings",
-              ),
-            ],
-          ),
+    return Center(
+      child: Text(
+        "Your Informations",
+        style: GoogleFonts.poppins(
+          fontSize: 20,
+          color: Colors.black,
+          fontWeight: FontWeight.w400,
         ),
       ),
     );
